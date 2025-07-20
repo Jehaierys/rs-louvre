346c0652d2a6f82dc7d4fc9434830386d1259311
Jehaierys <kawaysempai@gmail.com>
2025-07-20 21:57:19 +0500

    refactor: class name changes

--- scrypt/video.js
+++ scrypt/video.js
@@ -21,3 +21,59 @@ function initializeVideoSection() {
 }
 
 arrangeVideoListeners();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let players = {};
+
+function onYouTubeIframeAPIReady() {
+    const videos = ['VIDEO_LARGE', 'VIDEO_PETITE_2', 'VIDEO_PETITE_3', 'VIDEO_PETITE_4'];
+
+    for (let i = 0; i < videos.length; ++i) {
+        players[i] = new YT.Player(videos[i], {
+            videoId: videos[i],
+            playerVars: { controls: 0, modestbranding: 1 },
+            events: {
+                onReady: onPlayerReady,
+                onStateChange: onPlayerStateChange
+            }
+        });
+    }
+}
