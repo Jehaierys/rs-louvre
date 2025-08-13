c695753851da490612441d623fe7bd3db3c3c256
Jehaierys <kawaysempai@gmail.com>
2025-08-13 13:14:18 +0500

    refactor: cancel video

--- scrypt/Bootstrap.js
+++ scrypt/Bootstrap.js
@@ -1,12 +1,12 @@
 window.SCALE = {PX_1920: 1920, PX_1024: 1024, PX_768: 768, PX_420: 420};
 window.currentScale;
 
-const videoDispatcher = new VideoDispatcher();
+// const videoDispatcher = new VideoDispatcher();
 const welcomeSlider = new WelcomeSlider();
 const galleryArranger = new GalleryArranger();
 const galleryAnimator = new GalleryAnimator();
 
-window.onYouTubeIframeAPIReady = videoDispatcher.onYouTubeIframeAPIReady.bind(videoDispatcher);
+// window.onYouTubeIframeAPIReady = videoDispatcher.onYouTubeIframeAPIReady.bind(videoDispatcher);
 
 document.addEventListener('DOMContentLoaded', () => {
     NavigationMenuFacade.addScrollLimiter();
@@ -20,6 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     welcomeSlider.initializeTouchSwiper();
 
-    videoDispatcher.addWindowWidthAdapter();
-    videoDispatcher.arrange();
+    // videoDispatcher.addWindowWidthAdapter();
+    // videoDispatcher.arrange();
 });
